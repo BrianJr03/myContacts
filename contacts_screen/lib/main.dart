ca538399dc0a6d599e4b1a8b654f4b427e091a81
Brian Walker Jr <thaballa79@gmail.com>
2022-05-16 14:52:09 -0400

    removes unused code

--- contacts_screen/lib/main.dart
+++ contacts_screen/lib/main.dart
@@ -8,10 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'myContacts',
-      theme: ThemeData(),
-      home: const ContactsPage(),
+      home: ContactsPage(),
       debugShowCheckedModeBanner: false,
     );
   }
